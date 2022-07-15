02f608b9d17344c6aea6a29ba408cb979b805c36
Waleed1100x <107954669+Waleed1100x@users.noreply.github.com>
2022-07-15 13:45:10 +0300

    Compare Task Done

--- assets/js/scrpit.js
+++ assets/js/scrpit.js
@@ -13,19 +13,20 @@ inputBox.onkeyup = (e)=>{
     let emptyArray = [];
     if(userData){
         icon.onclick = ()=>{
-            
             buildTable(userData.toLocaleLowerCase());
         }
         emptyArray = phone.filter((data)=>{
             //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+        
             return data.name.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
         });
         emptyArray = emptyArray.map((data)=>{
             // passing return data inside li tag
-            return data.name = `<li>${data.name}</li>`;
+           
+            return  `<li>${data.name}</li>`;
         });
         searchWrapper.classList.add("active"); //show autocomplete box
-        showSuggestions(emptyArray);
+        showSuggestions(emptyArray); 
         let allList = suggBox.querySelectorAll("li");
         for (let i = 0; i < allList.length; i++) {
             //adding onclick attribute in all li tag
@@ -58,7 +59,7 @@ function showSuggestions(list){
 // Function buildTable
 function buildTable(search){
 
-    let s = `<li>${search}</li>`;
+    let s = `${search}`;
 let table = document.getElementById("table-group-divider");
 
     for(var i = 0; i< phone.length ; i++){
@@ -67,20 +68,29 @@ let table = document.getElementById("table-group-divider");
         let j = 0;
         if(ComparePhones.length == 2){
         var row = `
-        <tr>
-        <th scope="col">Brand</th>
-        <th scope="col">${ComparePhones[j].brand}</th>
-        <th scope="col">${ComparePhones[j+1].brand}</th>
-        </tr>
+            <tr>
+            <th scope="col"></th>
+            <th scope="col"><img src="./assets/img/${ComparePhones[j].name}.jpg" alt=""></th>
+            <th scope="col"><img src="./assets/img/${ComparePhones[j+1].name}.jpg" alt=""></th>
+            </tr>
+            <tr>
+            <th scope="col">Brand</th>
+            <th scope="col">${ComparePhones[j].brand}</th>
+            <th scope="col">${ComparePhones[j+1].brand}</th>
+            </tr>
         <tr> 
         <th scope="col">Name</th>
         <td>${ComparePhones[j].name}</td>
         <td>${ComparePhones[j+1].name}</td>
         </tr>
         <tr> 
-        <th scope="col">Battery</th>
-        <td>${ComparePhones[j].Battery}</td>
-        <td>${ComparePhones[j+1].Battery}</td>
+        <th scope="col">Released</th>
+        <td>${ComparePhones[j].Released}</td>
+        <td>${ComparePhones[j+1].Released}</td>
+        </tr>
+        <th scope="col">Connectivity</th>
+        <td>${ComparePhones[j].Connectivity}</td>
+        <td>${ComparePhones[j+1].Connectivity}</td>
         </tr>
         <tr> 
         <th scope="col">Size</th>
@@ -88,9 +98,18 @@ let table = document.getElementById("table-group-divider");
         <td>${ComparePhones[j+1].Size}</td>
         </tr>
         <tr> 
-        <th scope="col">Released</th>
-        <td>${ComparePhones[j].Released}</td>
-        <td>${ComparePhones[j+1].Released}</td>
+        <th scope="col">Memory</th>
+        <td>${ComparePhones[j].Memory}</td>
+        <td>${ComparePhones[j+1].Memory}</td>
+        </tr>
+        <th scope="col">Resolution</th>
+        <td>${ComparePhones[j].Resolution}</td>
+        <td>${ComparePhones[j+1].Resolution}</td>
+        </tr>
+        <tr> 
+        <th scope="col">Battery</th>
+        <td>${ComparePhones[j].Battery} mAh</td>
+        <td>${ComparePhones[j+1].Battery} mAh</td>
         </tr>
         `
         ComparePhones = [];
@@ -98,7 +117,10 @@ let table = document.getElementById("table-group-divider");
         table.innerHTML += row
         } else {
             var row = `
-            
+            <tr>
+            <th scope="col"></th>
+            <th scope="col"><img src="./assets/img/${ComparePhones[j].name}.jpg" alt=""></th>
+           </tr>
             <tr>
             <th scope="col">Brand</th>
             <th scope="col">${ComparePhones[j].brand}</th>
@@ -108,16 +130,28 @@ let table = document.getElementById("table-group-divider");
             <td>${ComparePhones[j].name}</td>
             </tr>
             <tr> 
-            <th scope="col">Battery</th>
-            <td>${ComparePhones[j].Battery}</td>
+            <th scope="col">Released</th>
+            <td>${ComparePhones[j].Released}</td>
+            </tr>
+            <th scope="col">Connectivity</th>
+            <td>${ComparePhones[j].Connectivity}</td>
             </tr>
             <tr> 
             <th scope="col">Size</th>
             <td>${ComparePhones[j].Size}</td>
             </tr>
             <tr> 
-            <th scope="col">Released</th>
-            <td>${ComparePhones[j].Released}</td>
+            <th scope="col">Memory</th>
+            <td>${ComparePhones[j].Memory}</td>
+            </tr>
+            <tr> 
+            <th scope="col">Resolution</th>
+            <td>${ComparePhones[j].Resolution}</td>
+            </tr>
+            <tr> 
+            <th scope="col">Battery</th>
+            <td>${ComparePhones[j].Battery} mAh</td>
+
             </tr>
             `
             table.innerHTML = "";
